b134429ff9d824bbe36c43b40f875dd181588703
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-12-06 16:01:46 +0200

    program.cs

--- Program.cs
+++ Program.cs
@@ -1,35 +1,38 @@
-public class Program
+namespace DemoMvc
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        //CreateHostBuilder(args).Build().Run();
-
-        var builder = WebApplication.CreateBuilder();
-
-        // ConfigureServices => builder.Services
-
-        var app = builder.Build();
-
-        #region Configure
-        if (app.Environment.IsDevelopment())
+        public static void Main(string[] args)
         {
-            // ...
+            //CreateHostBuilder(args).Build().Run();
+
+            var builder = WebApplication.CreateBuilder();
+
+            // ConfigureServices => builder.Services
+
+            var app = builder.Build();
+
+            #region Configure
+            if (app.Environment.IsDevelopment())
+            {
+                // ...
+            }
+
+            app.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+            #endregion
+
+            app.Run();
         }
 
-        app.MapGet("/", async context =>
-        {
-            await context.Response.WriteAsync("Hello World!");
-        });
-
-        #endregion
-
-        app.Run();
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //             webBuilder.UseStartup<Startup>();
+        //        });
     }
-
-    //public static IHostBuilder CreateHostBuilder(string[] args) =>
-    //    Host.CreateDefaultBuilder(args)
-    //        .ConfigureWebHostDefaults(webBuilder =>
-    //        {
-    //             webBuilder.UseStartup<Startup>();
-    //        });
 }
